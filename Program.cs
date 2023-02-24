f483901bc7d462adc89c6379204999ee4d4bb0e9
Andrey <shakht_a@mail.ru>
2023-02-24 11:56:09 +0300

    задача 8 печать четных чисел, не превышающих заданное

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Ввод числа
+
+Console.WriteLine("Введите число");
+
+string Str = Console.ReadLine() ?? "";
+int Num = int.Parse(Str);
+
+// Печать четных чисел, не превышающих Num
+
+int i = 2;
+Console.Write($"{Num} -> {i}");
+while (i < Num-1)
+    {
+        i +=2;
+        Console.Write($", {i}");
+        //i +=2;
+    }
