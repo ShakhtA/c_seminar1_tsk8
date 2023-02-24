f5a592bce738b6d0d2a2ecec08ed03cb8d2514ba
Andrey <shakht_a@mail.ru>
2023-02-24 17:54:11 +0300

    небольшие правки

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Ввод числа
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите положительное целое число");
 
 string Str = Console.ReadLine() ?? "";
 int Num = int.Parse(Str);
@@ -8,10 +8,20 @@ int Num = int.Parse(Str);
 // Печать четных чисел, не превышающих Num
 
 int i = 2;
-Console.Write($"{Num} -> {i}");
-while (i < Num-1)
+if (Num < 2)
+    Console.Write($"{Num} -> ");
+else
     {
-        i +=2;
-        Console.Write($", {i}");
-        //i +=2;
+        Console.Write($"{Num} -> {i}");
+        while (i < Num)
+            {
+                Console.Write($", {i+=2}");
+                i +=2;
+            }
     }
+//Console.Write($"{Num} -> {i}");
+//while (i < Num + 1)
+//  {
+//        Console.Write($", {i+=2}");
+//          i +=2;
+//  }
